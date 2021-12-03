d7a0f448cea3203cf73b55975be404ac2ca5016a
petrekovc@gmail.com <petrekovc@gmail.com>
2021-12-03 20:05:59 +0100

    jjj

--- SpaceWarriors/Program.cs
+++ SpaceWarriors/Program.cs
@@ -6,7 +6,15 @@ namespace SpaceWarriors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            Console.WriteLine("zadejte 1. cislo");
+            int cisloA = int.Parse(Console.ReadLine());
+            Console.WriteLine("zadejte 2. cislo");
+            int cisloB = int.Parse(Console.ReadLine());
+            int vysledek = cisloA + cisloB;
+            Console.WriteLine("vysledek je " + vysledek.ToString());
+
+            Console.ReadLine();
         }
     }
 }
