7b1a1a7fd69852bc496df54b7282f011c55587bd
petrekovc@gmail.com <petrekovc@gmail.com>
2021-12-03 18:53:33 +0100

    hh

---
+++ SpaceWarriors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpaceWarriors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
